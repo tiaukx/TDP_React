78fb72348f533dc361dcfa62a64154a99e63cc47
tiaukx <47751992+tiaukx@users.noreply.github.com>
2022-06-14 12:27:43 +0100

    Update App.js

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
-import AddressApp from './address';
+// import AddressApp from './address';
 import Header from './Header';
-import AnotherComponent from './Footer';
-import PropPractice from './Props_practice';
+import MilesAhead from './MilesAhead';
+// import AnotherComponent from './Footer';
+// import PropPractice from './Props_practice';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <AddressApp/>
-      
+      {/* <AddressApp/>
       <PropPractice name="Moogle" age={6} likes="Cable ties" dislikes="Mickey"/>
       <PropPractice name="Mickey" age={5} likes="Belly rubs" dislikes="Moogle's tail"/>
+      <AnotherComponent/> */}
 
-      <AnotherComponent/>
+      <MilesAhead/>
+
     </div>
   );
 }
