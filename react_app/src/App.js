80e9dab82306751954f357971de085d560693071
tiaukx <47751992+tiaukx@users.noreply.github.com>
2022-06-15 16:13:19 +0100

    re-organised files

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,26 +1,15 @@
 import './App.css';
-// import GradeResults from './Grades';
-// import Counter from './Counter';
-// import CarDetails from './CarComponents';
-// import CustomerDetails from './CustomerForm';
-// import AddressApp from './address';
 import Header from './Header';
-import SubContent from './SubContent';
-// import Content from './ReactStaticDataPractice';
-// import HigherFunctionPractice from './HigherFunctionPractice';
-// import ProductTable from './SearchValue';
-// import MilesAhead from './MilesAhead';
-// import Clock from './Clock';
-// import AnotherComponent from './Footer';
-// import PropPractice from './Props_practice';
+
+import LoginControl from './Login_Control_Exercise/LoginControl';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
 
-      {/* <Content/> */}
-      <SubContent/>
+      <LoginControl/>
 
     </div>
   );
---
+++ react_app/src/Login_Control_Exercise/LoginControl.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import Greeting from './Greeting';
+
+const LoginControl = () => {
+
+    const [isloggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <>
+
+        </>
+    )
+
+}
+
+export default LoginControl;
