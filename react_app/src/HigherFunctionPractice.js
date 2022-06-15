2213ab92f3587ef5836d1b76c7cf03d96959e6f1
tiaukx <47751992+tiaukx@users.noreply.github.com>
2022-06-15 14:22:22 +0100

    Update HigherFunctionPractice.js

--- react_app/src/HigherFunctionPractice.js
+++ react_app/src/HigherFunctionPractice.js
@@ -1,15 +1,19 @@
 const HigherFunctionPractice = () => {
     
-    const numArray = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const numArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
     const maxValue = numArray.reduce((acc, next) => (acc > next) ? acc : next);
+    //alternatively could also be done using
+    // const maxValue = numArray.reduce((acc, next) => Math.max(acc, next));
     const minValue = numArray.reduce((acc, next) => (acc < next) ? acc : next);
 
-    const noOddNums = numArray.filter(num => {return num % 2 === 0}).reduce((acc, next) => acc + ", " + next);
-    const noEvenNums = numArray.filter(num => {return num % 2 !== 0}).reduce((acc, next) => acc + ", " + next);
+    const noOddNums = numArray.filter(num => num % 2 === 0).reduce((acc, next) => acc + ", " + next);
+    const noEvenNums = numArray.filter(num => num % 2 !== 0).reduce((acc, next) => acc + ", " + next);
 
-    const sumOfNums = numArray.reduce((acc, next) => {return acc + next});
-    const squareOfNums = numArray.map(num => num ** 2).filter(num => {return num % 2 !== 0}).reduce((acc, next) => (acc < next) ? acc : next);
+    const sumOfNums = numArray.reduce((acc, next) => acc + next);
+    const squareOfNums = numArray.map(num => num ** 2).filter(num => num % 2 !== 0).reduce((acc, next) => (acc < next) ? acc : next);
+    //alternatively could also be done using Math.pow
+    //const squareOfNums = numArray.map(num => Math.pow(num, 2)).filter(num => num % 2 !== 0).reduce((acc, next) => (acc < next) ? acc : next);
 
     return (
         <>
