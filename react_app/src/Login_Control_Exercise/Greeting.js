e7407883da086c310cbf9244d17fe8e4457bfe5a
tiaukx <47751992+tiaukx@users.noreply.github.com>
2022-06-16 16:17:12 +0100

    add notes to login control

--- react_app/src/Login_Control_Exercise/Greeting.js
+++ react_app/src/Login_Control_Exercise/Greeting.js
@@ -1,6 +1,9 @@
 const Greeting = (props) => {
+    //creates a new constant that uses the isLoggedIn value from the relevant prop
     const isloggedIn = props.isloggedIn;
+    //if isLoggedIn is true, returns a greeting
     if (isloggedIn) return <h1>Welcome Back!</h1>
+    //if isLoggedIn is false, returns a sign up request
     return <h1>Please sign up.</h1>
 }
 
--- react_app/src/Login_Control_Exercise/LoginControl.js
+++ react_app/src/Login_Control_Exercise/LoginControl.js
@@ -5,13 +5,19 @@ import Greeting from './Greeting';
 
 const LoginControl = () => {
 
+    //sets isLoggedIn to false as default
     const [isloggedIn, setIsLoggedIn] = useState(false);
 
+    //button that changes its value depending on the value of isLoggedIn, using an if statement
+    //if isLoggedIn is true, displays the logout button
+    //if isLoggedIn is false, displays the login button
     let button = (isloggedIn) ? <LogoutButton onClick={() => setIsLoggedIn(false)}/> : <LoginButton onClick={() => setIsLoggedIn(true)}/>
 
     return (
         <>
+            {/* returns the greeting prop if isLoggedIn is true */}
             <Greeting isloggedIn={isloggedIn}/>
+            {/* renders button depending on isLoggedIn value */}
             {button}
         </>
     )
