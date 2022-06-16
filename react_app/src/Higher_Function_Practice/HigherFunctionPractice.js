1ebe7fe515cfb08614e1b889ad11bbeb4e3e22cd
tiaukx <47751992+tiaukx@users.noreply.github.com>
2022-06-16 16:20:04 +0100

    updated higher function notes

--- react_app/src/Higher_Function_Practice/HigherFunctionPractice.js
+++ react_app/src/Higher_Function_Practice/HigherFunctionPractice.js
@@ -2,6 +2,8 @@ const HigherFunctionPractice = () => {
     
     const numArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
+    //reduces the value of an array to one value, acc & next used to cycle through each element in array and perform a task
+    //in this case, finding the larger value then discarding the smaller value
     const maxValue = numArray.reduce((acc, next) => (acc > next) ? acc : next);
     //alternatively could also be done using
     // const maxValue = numArray.reduce((acc, next) => Math.max(acc, next));
@@ -14,6 +16,7 @@ const HigherFunctionPractice = () => {
     const squareOfNums = numArray.map(num => num ** 2).filter(num => num % 2 !== 0).reduce((acc, next) => (acc < next) ? acc : next);
     //alternatively could also be done using Math.pow
     //const squareOfNums = numArray.map(num => Math.pow(num, 2)).filter(num => num % 2 !== 0).reduce((acc, next) => (acc < next) ? acc : next);
+    //with Math.pow(num, 2) you have to state the 2nd value (e.g. 2) to say what you want the power to be
 
     return (
         <>
