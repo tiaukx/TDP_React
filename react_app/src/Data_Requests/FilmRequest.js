2d033825e4eba6d672eab69f4e011dfdbd31ec91
tiaukx <47751992+tiaukx@users.noreply.github.com>
2022-06-17 11:10:54 +0100

    messing with use effect

--- react_app/src/Data_Requests/FilmRequest.js
+++ react_app/src/Data_Requests/FilmRequest.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import FilmInfo from './FilmInfo';
 
 
@@ -7,17 +7,17 @@ const FilmRequest = () => {
 
     const [data, setData] = useState("");
     const [apiKey, setApiKey] = useState("");
-    const [filmTitle, setFilmTitle] = useState("");
+    const [filmTitle, setFilmTitle] = useState([]);
     const [request, setRequest] = useState(false);
 
     //destructuring so we don't have to type out data.Title, etc.
-    const {Title, Year, Rated, Genre, Plot, Poster} = data;
+    const { Title, Year, Rated, Genre, Plot, Poster } = data;
 
     //Renders the film information when request is turned to true, which is done when button is clicked
     const render = () => {
         if (request) {
             //Calls information from the filmInfo prop, that states the format when rendered
-            return <FilmInfo key={Title + Year} Title={Title} Year={Year} Rated={Rated} Genre={Genre} Plot={Plot} Poster={Poster}/>
+            return <FilmInfo key={Title + Year} Title={Title} Year={Year} Rated={Rated} Genre={Genre} Plot={Plot} Poster={Poster} />
         }
     }
 
@@ -50,19 +50,33 @@ const FilmRequest = () => {
             })
     };
 
+    //another way to make a request for film data, but loads results as you type
+    useEffect(() => {
+        axios
+            //uses axios to get data from API, takes the base website, adds user API key, next part of URL and film title
+            .get("http://www.omdbapi.com/?i=tt3896198&apikey=" + apiKey + "&t=" + filmTitle)
+            .then(response => {
+                console.log(response.data);
+                //sets the data state we've set to the response data from the API
+                setData(response.data);
+                //sets the request to true to render details onto page
+                setRequest(true);
+            })
+    }, [filmTitle]);
+
     return (
         <>
             <fieldset>
                 <h2>Film Request</h2>
                 <p>Enter API Key:</p>
                 {/* when changed (as user types) calls the function that changes the API key */}
-                <input type='text' onChange={(event)=>updateApiKey(event)}/>    
+                <input type='text' onChange={(event) => updateApiKey(event)} />
                 <p>Enter name of film:</p>
-                                {/* when changed (as user types) calls the function that changes the film title */}
-                <input type='text' onChange={(event)=>updateTitle(event)}/>
+                {/* when changed (as user types) calls the function that changes the film title */}
+                <input type='text' onChange={(event) => updateTitle(event)} />
 
                 {/* when clicked calls the function that requests the data from the API */}
-                <button onClick={(event)=>makeRequest(event)}>Search</button>
+                <button onClick={(event) => makeRequest(event)}>Search</button>
 
                 {/* renders the film details when request is made */}
                 {render()}
